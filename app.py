46cf952be086a3484aa06ca4efa43af2a4edcc56
hcart15 <harry15carter@gmail.com>
2025-03-04 14:34:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -232,4 +232,4 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
